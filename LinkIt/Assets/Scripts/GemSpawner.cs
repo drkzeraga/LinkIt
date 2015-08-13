928f8a9b57a700e13d0bffd0bb23be0b99c1ba87
drkzeraga <zeraga@hotmail.com>
2015-08-13 19:12:39 +0800

    Added partial spawn logic

--- LinkIt/Assets/Scripts/GemSpawner.cs
+++ LinkIt/Assets/Scripts/GemSpawner.cs
@@ -1,16 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class GemSpawner : MonoBehaviour 
 {
 	public GameObject[] mGemTypes;
-	public int mLaneCount = 1;
-	public float mSpawnInterval = 1.0f;
-	public float mDropSpeed = 1.0f;
+	public int   mLaneCount;
+	public float mSpawnFrequency;
+	public float mDropSpeed;
+    public float mLaneCooldown;
 
-	private float mTimeElapsed = 0.0f;
 	private LinkedList< GameObject > mGems = new LinkedList< GameObject > ();
+	private float   mSpawnCounter = 0.0f;
+    private float[] mLaneTimerArr;
+    private bool    mIsLaneFull;
+
+    const int       INVALID_LANE = -1;
+
+
+    //DEBUG STUFF
+    const float     SPAWN_FREQ_DELTA = 0.25f;
+    const float     DROP_SPEED_DELTA = 0.1f;
+
+    public Text     spawnFreqText;
+    public Text     dropSpeedText;
 
     // Get All Gems
     public LinkedList< GameObject > GetAllGems ()
@@ -29,6 +43,9 @@ public class GemSpawner : MonoBehaviour
                 g.mType = i;
         }
 
+        //Init lane cooldow
+        InitLane();
+
         //HACKKK: Seeding with a default value
         Random.seed = 0;
 	}
@@ -36,14 +53,20 @@ public class GemSpawner : MonoBehaviour
 	// Update is called once per frame
 	void FixedUpdate () 
 	{
-		UpdateGems ();
+        //Handle input
+        HandleInput();
 
-		mTimeElapsed +=  Time.fixedDeltaTime;
-		if( mTimeElapsed >= mSpawnInterval )
-		{
-			Spawn ();
-			mTimeElapsed -= mSpawnInterval;
-		}
+        //Update the lane
+        UpdateLane();
+
+        //Update the spawn
+        UpdateSpawn();
+
+        //Update the gems
+		UpdateGems();
+
+        //Update debug text
+        UpdateDebugText();
 	}
 
 	// Get the next colour gem
@@ -55,8 +78,44 @@ public class GemSpawner : MonoBehaviour
 	// Get the next lane number
 	int GetNextLane()
 	{
-		return Random.Range ( 1, Mathf.Max( 1, mLaneCount + 1 ) );
+        //Get an intial random lane
+        int randLane = Random.Range ( 0, Mathf.Max( 0, mLaneCount) );
+        
+        //This is the lane to be returned
+        int choosenLane = randLane;
+
+        //If the choosen lane is on cooldown
+        while (IsLaneOnCooldown(choosenLane))
+        {
+            //Increment to next lane
+            ++choosenLane;
+
+            //If choosen lane exceed lane limit
+            if (choosenLane == mLaneCount)
+            {
+                //Wrap around back to start
+                choosenLane = 0;
+            }
+
+            //If choosen lane is back to initial lane
+            if (choosenLane == randLane)
+            {
+                //Then no lanes is avaliable,
+                // return invalid lane
+                return INVALID_LANE;
+            }
+        }
+
+        //Else if the lane is avaliable
+        // return lane
+        return choosenLane;
 	}
+
+    bool IsLaneOnCooldown(int lane)
+    {
+        Debug.Log(lane);
+        return mLaneTimerArr[lane] > 0.0f;
+    }
 
 	// Get the camera view height in world space
 	float GetWorldHeight()
@@ -71,6 +130,21 @@ public class GemSpawner : MonoBehaviour
 		}
 	}
 
+    //Update the spawn logic
+    void UpdateSpawn()
+    {
+        //Increment spawn counter
+        mSpawnCounter += Time.fixedDeltaTime * mSpawnFrequency;
+        
+        //While there is still gems remaining to be spawn
+        // and lanes are still not full
+        while (mSpawnCounter >= 1.0f && !mIsLaneFull)
+        {
+            //Spawn a new gem
+            Spawn();
+        }
+    }
+
 	// Spawn logic
 	void Spawn ()
 	{
@@ -78,18 +152,30 @@ public class GemSpawner : MonoBehaviour
 		float height = GetWorldHeight();
 		float width = Camera.main.aspect * height;
 
-		int c = GetNextGem ();
-		int l = GetNextLane ();
+		int color = GetNextGem ();
+		int lane = GetNextLane ();
+
+        //If the lane return is invalid,
+        // aka there is no lane free
+        if (lane == INVALID_LANE)
+        {
+            //Set flag to true
+            mIsLaneFull = true;
+            return;
+        }
 
 		// 0.1f -> pad
 		float minSpawnX = -( width - 1.0f );
 		float spawnWidth = -2.0f * minSpawnX;
 
-		float offset = minSpawnX + ( float )l * spawnWidth / ( float )( mLaneCount + 1 );
+		float offset = minSpawnX + ( float )lane * spawnWidth / ( float )( mLaneCount + 1 );
 		Vector3 spawnPos = Vector3.up * height+
 						   Vector3.right * offset;
 
-		mGems.AddLast( ( GameObject )( Instantiate ( mGemTypes [ c ], spawnPos, Quaternion.identity ) ) );
+		mGems.AddLast( ( GameObject )( Instantiate ( mGemTypes [ color ], spawnPos, Quaternion.identity ) ) );
+
+        //Decrement spawn counter
+        mSpawnCounter -= 1.0f;
 	}
 
 	void UpdateGems ()
@@ -142,4 +228,67 @@ public class GemSpawner : MonoBehaviour
 			}
 		}
 	}
+
+    void InitLane()
+    {
+        //Allocate memeory
+        mLaneTimerArr = new float[mLaneCount];
+
+        //Clean the array
+        for (int i = 0; i < mLaneCount; ++i)
+        {
+            mLaneTimerArr[i] = 0.0f;
+        }
+
+        //Set lane flag to false
+        mIsLaneFull = false;
+    }
+
+    void UpdateLane()
+    {
+        //Reset the lane flag
+        mIsLaneFull = false;
+
+        //Iteriate throught all lanes
+        for (int i = 0; i < mLaneCount; ++i)
+        {
+            //Decrement lane timer
+            mLaneTimerArr[i] -= Time.fixedDeltaTime;
+        }
+    }
+
+    void HandleInput()
+    {
+        //Handle input for debug functions
+        HandleDebugInput();
+    }
+
+    void HandleDebugInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Equals))
+        {
+            mSpawnFrequency += SPAWN_FREQ_DELTA;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Minus))
+        {
+            mSpawnFrequency -= SPAWN_FREQ_DELTA;
+        }
+
+        if (Input.GetKeyDown(KeyCode.PageUp))
+        {
+            mDropSpeed += DROP_SPEED_DELTA;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.PageDown))
+        {
+            mDropSpeed -= DROP_SPEED_DELTA;
+        }
+    }
+
+    void UpdateDebugText()
+    {
+        spawnFreqText.text = "SpawnFreq: " + mSpawnFrequency;
+        dropSpeedText.text = "DropSpeed: " + mDropSpeed;
+    }
 }
